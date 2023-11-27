7102f234742068c75d3bf8afaf6c612ac0ba9a39
anoushkachouksey <anoushkachouksey@gmail.com>
2023-11-27 20:08:25 +0530

    testing

---
+++ mutation-testing-integration/src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -0,0 +1,36 @@
+package com.javatechie.crud.example;
+
+import com.javatechie.crud.example.entity.Product;
+import com.javatechie.crud.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@Service
+public class SpringBootCrudExample2Application {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @PostConstruct
+    public void init() {
+//        repository.saveAll(
+//                Stream
+//                        .of(
+//                                new Product("Book", 1, 299),
+//                                new Product("mobile", 1, 39999))
+//                        .collect(Collectors.toList())
+//        );
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudExample2Application.class, args);
+    }
+
+}
